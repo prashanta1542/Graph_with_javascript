2819910116795e1c0f65aa85f7bcfbb6b5e2e501
Prashanta Chowdhury <prashantachowdhury967@gmail.com>
2024-05-17 00:47:43 +0600

    Update Find_ALl_Path_Source_to_Terget.js

--- Find_ALl_Path_Source_to_Terget.js
+++ Find_ALl_Path_Source_to_Terget.js
@@ -1,49 +1,49 @@
-// Online Javascript Editor for free
-// Write, Edit and Run your Javascript code using JS Online Compiler
 class Graph{
     constructor(){
-        this.adjacent={}
+        this.adjacentList={}
+        // this.v=[]
     }
+    
     addNode(node){
-        if(!this.adjacent[node]){this.adjacent[node]=[]}
+        if(!this.adjacentList[node]){this.adjacentList[node]=[]}
     }
-    addConnect(x1,x2){
-        this.adjacent[x1].push(x2)
-        this.adjacent[x2].push(x1)
+    connectNode(node,...prop){
+        this.adjacentList[node].push(...prop)
+    }
+    dfs(curr,v,path){
+
+        console.log(path)
+        v.push(curr)
+        for(let i=0;i<this.adjacentList[curr].length;i++){
+           
+            if(!v.includes(this.adjacentList[curr][i])){
+                this.dfs(this.adjacentList[curr][i],v,path+this.adjacentList[curr][i])
+            }
+        }
+     
+    }
+
+    findAllPath(curr,v,path,ter){
+        if(curr === ter){
+             console.log(path)
+             return
+        }
+        
+        
+        for(let i=0;i<this.adjacentList[curr].length;i++){
+            
+            if(!v.includes(this.adjacentList[curr][i])){
+                v.push(curr)
+                this.findAllPath(this.adjacentList[curr][i],v,path+this.adjacentList[curr][i],ter)
+                v.shift(curr)
+            }
+            
+        }
+     
     }
     
 }
-
-function dfs(graph,curr,vis){
-
-    console.log(curr)
-    vis.push(curr)
-  for(let i=0;i<graph.adjacent[curr].length;i++){
-    neighbour=graph.adjacent[curr][i]
-      if(!vis.includes(neighbour)){
-          dfs(graph,neighbour,vis)
-      }
-  }
-}
-
-function findShortestPath(graph,curr,vis,path,target){
-    if(curr==target){
-        console.log(path)
-        return
-    }
-
-    for(let i=0;i<graph.adjacent[curr].length;i++){
-        neighboures=graph.adjacent[curr][i];
-        if(!vis.includes(neighboures)){
-            vis.push(neighboures)
-            findShortestPath(graph,neighboures,vis,path+neighboures,target)
-            vis.shift(neighboures)
-        }
-    }
-}
-
 myGraph=new Graph()
-v=[]
 myGraph.addNode(0)
 myGraph.addNode(1)
 myGraph.addNode(2)
@@ -52,31 +52,15 @@ myGraph.addNode(4)
 myGraph.addNode(5)
 myGraph.addNode(6)
 
-myGraph.addConnect(0,1)
-myGraph.addConnect(0,2)
-myGraph.addConnect(1,3)
-myGraph.addConnect(2,4)
-myGraph.addConnect(3,4)
-myGraph.addConnect(3,5)
-myGraph.addConnect(4,5)
-myGraph.addConnect(5,6)
+v=[]
+myGraph.connectNode(0,1,2)
+myGraph.connectNode(1,0,3)
+myGraph.connectNode(2,0,4)
+myGraph.connectNode(3,1,4,5)
+myGraph.connectNode(4,2,3,5)
+myGraph.connectNode(5,3,4,6)
+myGraph.connectNode(6,5)
 
-findShortestPath(myGraph,0,v,'0',5)
+myGraph.findAllPath(0,v,'0',5)
 
-
-/* 
-       OUTPUT 
-0102435
-013135
-0134345
-0201345
-020245
-024245
-0243435
-
-
-*/
-
-
-
-
+console.log(myGraph)
