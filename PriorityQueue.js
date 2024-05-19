9a62a5655a58c495450fe153a46368f0c0eab05f
Prashanta Chowdhury <prashantachowdhury967@gmail.com>
2024-05-20 04:35:50 +0600

    Update PriorityQueue.js

--- PriorityQueue.js
+++ PriorityQueue.js
@@ -16,6 +16,19 @@ class PriorityQueue{
             this.items.push(item)
         }
     }
+    peek(){
+        return this.items[0]
+    }
+    dequeue(){
+        console.log(this.items.shift())
+    }
+    isEmpty(){
+       if(this.items.length == 0){
+        return true
+       }else{
+         return false
+       }
+    }
 }
 
 myQueue=new PriorityQueue()
@@ -27,5 +40,17 @@ myQueue.enqueue("D",5)
 myQueue.enqueue("E",2.5)
 myQueue.enqueue("E",3.7)
 
+console.log("Peek value is : " , myQueue.peek())
+
+myQueue.dequeue()
+myQueue.dequeue()
+myQueue.dequeue()
+myQueue.dequeue()
+myQueue.dequeue()
+myQueue.dequeue()
+
+if(myQueue.isEmpty()){
+    console.log("Queue is empty")
+}
 
 console.log(myQueue)
