8507402b410e25d2d799b7e744bc511fac87f08c
Prashanta Chowdhury <prashantachowdhury967@gmail.com>
2024-05-22 03:59:46 +0600

    Update Dijkastra_algorithm.js

--- Dijkastra_algorithm.js
+++ Dijkastra_algorithm.js
@@ -1,125 +1,162 @@
-class PriorityQueue {
-    constructor() {
-        this.items = [];
+class PriorityQueue{
+    constructor(){
+        this.items=[]
     }
-
-    enQueue(element, priority) {
-        const item = { element, priority };
-        let added = false;
-
-        for (let i = 0; i < this.items.length; i++) {
-            if (item.priority < this.items[i].priority) {
-                this.items.splice(i, 0, item);
-                added = true;
-                break;
+    enQueue(element,priority){
+        const item={element,priority}
+        let add=false
+        for(let i=0;i<this.items.length;i++){
+            if(item.priority<this.items[i].priority){
+                this.items.splice(i,0,item)
+                add=true
+                break
             }
         }
-        if (!added) {
-            this.items.push(item);
+        if(!add){
+            this.items.push(item)
         }
     }
 
-    deQueue() {
-        return this.items.shift();
+    isEmpty(){
+        if(this.items.length == 0){
+            return true
+        }else{
+            return false
+        }
     }
 
-    isEmpty() {
-        return this.items.length === 0;
+    deQueue(){
+       return  this.items.shift()
     }
 }
 
-
-
-class Graph {
-    constructor() {
-        this.adjacent = [];
+class Graph{
+    constructor(){
+        this.adjacent=[]
     }
-
-    addNode(node) {
-        if (!this.adjacent[node]) {
-            this.adjacent[node] = [];
+    addNode(node){
+        if(!this.adjacent[node])
+        {
+            this.adjacent[node]=[]
         }
     }
-
-    addEdge(node, des, w) {
-        this.adjacent[node].push([des, w]);
+    addEdge(src,des,weight){
+        this.adjacent[src].push([des,weight])
     }
+    dijKastra(src,vis,dis){
+        const pq=new PriorityQueue()
+        pq.enQueue(src,dis[0])
+        while(!pq.isEmpty()){
+            let pair=pq.deQueue()
 
-    printArray() {
-        for (let i = 0; i < Object.keys(this.adjacent).length; i++) {
-            for (let j = 0; j < this.adjacent[i].length; j++) {
-                console.log("Parent is:", i, "Child is:", this.adjacent[i][j]);
-            }
-        }
-    }
-
-    dijkstra(src, dis, vis) {
-        const pq = new PriorityQueue();
-        pq.enQueue(src, dis[0]);
-        while (!pq.isEmpty()) {
-            const curr = pq.deQueue().element;
-            if (!vis[curr]) {
-                vis[curr] = true;
-                for (let i = 0; i < this.adjacent[curr].length; i++) {
-                    const child = this.adjacent[curr][i];
-                    const u = curr;
-                    const v = child[0];
-                    const wt = child[1];
-                    if (dis[u] + wt < dis[v]) {
-                        dis[v] = dis[u] + wt;
-                        pq.enQueue(v, dis[v]);
+                    vis[pair.element]=true
+                    for(let i=0;i<this.adjacent[pair.element].length;i++){
+                        let edge=this.adjacent[pair.element]
+                        console.log(edge[i])               
+                        let u=pair.element
+                        let v=edge[i][0]
+                        let weight=edge[i][1]
+                        
+                        if(dis[u]+weight < dis[v]){
+                            dis[v]=dis[u]+weight
+                            pq.enQueue(v,dis[v])
+                        }
                     }
-                }
-            }
+                
+            
+
         }
-        for (let i = 0; i < dis.length; i++) {
-            console.log(`Distance from source to node ${i}: ${dis[i]}`);
+        console.log(dis)
+    }
+    printAllSiblingsOfNode(node){
+        for(let i=0;i<this.adjacent[node].length;i++){
+            console.log("Parent of ",this.adjacent[node][i])
+            for(let j=0;j<this.adjacent[node][i].length;j++){
+               if(j==0){
+                console.log("Destination : ",this.adjacent[node][i][j])
+               } else{
+                console.log("Weight : ",this.adjacent[node][i][j])
+               }
+            }
         }
     }
 }
+g= new Graph()
 
-const g = new Graph();
+g.addNode(0)
+g.addNode(1)
+g.addNode(2)
+g.addNode(3)
+g.addNode(4)
+g.addNode(5)
 
-const vis = [];
-const dis = [];
 
-g.addNode(0);
-g.addNode(1);
-g.addNode(2);
-g.addNode(3);
-g.addNode(4);
-g.addNode(5);
-g.addNode(6);
-g.addNode(7);
-g.addNode(8);
+//input 1 [ 0, 32, 12, 44, 9007199254740991 ]
+// g.addEdge(0,3,60)
+// g.addEdge(0,2,12)
 
-g.addEdge(0, 1, 4);
-g.addEdge(0, 7, 8);
+// g.addEdge(1,0,10)
 
-g.addEdge(1, 2, 8);
-g.addEdge(1, 7, 11);
+// g.addEdge(2,1,20)
+// g.addEdge(2,3,32)
 
-g.addEdge(2, 3, 7);
-g.addEdge(2, 5, 4);
-g.addEdge(2, 8, 2);
+// g.addEdge(4,0,7)
 
-g.addEdge(3, 4, 9);
-g.addEdge(3, 5, 14);
+//input 2 [ 0, 2, 3, 8, 6, 9 ]
+g.addEdge(0,1,2)
+g.addEdge(0,2,4)
 
-g.addEdge(4, 5, 10);
+g.addEdge(1,2,1)
+g.addEdge(1,3,7)
 
-g.addEdge(5, 6, 2);
+g.addEdge(2,4,3)
 
-g.addEdge(6, 8, 6);
-g.addEdge(6, 7, 1);
+g.addEdge(3,5,1)
 
-g.addEdge(7, 8, 7);
+g.addEdge(4,3,2)
+g.addEdge(4,5,5)
 
-const lTG = Object.keys(g.adjacent).length; // Here lTG is length of graph
+//input 3 output[ 0, 1, 2, 4, 2, 3 ]
+// g.addEdge(0,1,1)
+// g.addEdge(0,2,2)
 
-for (let i = 0; i < lTG; i++) {
-    vis[i] = false;
-    dis[i] = i === 0 ? 0 : Number.MAX_SAFE_INTEGER;
+// g.addEdge(1,0,1)
+// g.addEdge(1,3,5)
+// g.addEdge(1,4,1)
+
+
+// g.addEdge(2,1,2)
+// g.addEdge(2,3,2)
+// g.addEdge(2,4,3)
+
+// g.addEdge(3,1,5)
+// g.addEdge(3,2,2)
+// g.addEdge(3,4,2)
+// g.addEdge(3,5,2)
+
+// g.addEdge(4,1,1)
+// g.addEdge(4,2,3)
+// g.addEdge(4,3,2)
+// g.addEdge(4,5,1)
+
+// g.addEdge(5,3,2)
+// g.addEdge(5,4,1)
+
+
+//length of Graph 
+let ltg=Object.keys(g.adjacent).length
+//create an Visited array
+v=[]
+for(let i=0;i<ltg;i++){
+   v[i]=true
 }
+//create an Distance array
+d=[]
+for(let i=0;i<ltg;i++){
+     d[i]=Number.MAX_SAFE_INTEGER
+}
+d[0]=0
 
-g.dijkstra(0, dis, vis);
+// g.printAllSiblingsOfNode(0)
+g.dijKastra(0,v,d)
+console.log(g)
